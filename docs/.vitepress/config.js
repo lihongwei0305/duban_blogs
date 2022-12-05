b1ecf41c650ec0b9c9b5077c25987a24a0ede374
lihw <lihw@gov.in>
2022-12-05 14:03:43 +0800

    feat: 改结构

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,36 @@
+export const nav = [
+    {text: '前端', link: '/src/javaScript/array'},
+    {
+        text: '后端',
+        link: '/src/java/java',
+    },
+]
+
+export const sidebar = {
+    "/src/java/": [
+        {
+            text: '',
+            items: [
+                {text: 'java', link: '/src/java/java'},
+            ]
+        },
+    ],
+    "/src/javaScript": [
+        {
+            text: 'javaScript',
+            items: [
+                {text: 'Array', link: '/src/javaScript/array'},
+                {text: 'Object', link: '/src/javaScript/object'},
+                {text: 'Js高级', link: '/src/javaScript/jsAdvanced'},
+                {text: 'Dom', link: '/src/javaScript/dom'},
+                {text: 'Util', link: '/src/javaScript/util'},
+                {text: 'Library', link: '/src/javaScript/library'},
+            ]
+        },
+    ]
+
+}
+
 export default {
     title: 'Duban_Blog',
     description: 'Just playing around.',
@@ -7,25 +40,8 @@ export default {
         lineNumbers: true
     },
     themeConfig: {
-        sidebar: [
-            {
-                text: 'javaScript',
-                items: [
-                    {text: 'Array', link: '/src/javaScript/array'},
-                    {text: 'Object', link: '/src/javaScript/object'},
-                    {text: 'Js高级', link: '/src/javaScript/jsAdvanced'},
-                    {text: 'Dom', link: '/src/javaScript/dom'},
-                    {text: 'Util', link: '/src/javaScript/util'},
-                    {text: 'Library', link: '/src/javaScript/library'},
-                ]
-            },
-            {
-                text: '',
-                items: [
-                    {text: 'Vue', link: '/src/vue/vue3'},
-                ]
-            }
-        ],
+        nav,
+        sidebar,
         /*algolia: {
             indexName: 'vuejs',
             appId: 'ML0LEBN7FQ',
@@ -34,5 +50,7 @@ export default {
                 facetFilters: ['version:v3']
             }
         },*/
+
+
     }
 }
